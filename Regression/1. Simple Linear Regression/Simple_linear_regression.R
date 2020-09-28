854083c73b2aafaa630823fd541c6eeb20483e86
SARTHAK ASTHANA <sarthakasthana2014@gmail.com>
2020-09-28 22:54:52 +0530

    Linear Regression donee

--- Regression/1. Simple Linear Regression/Simple_linear_regression.R
+++ Regression/1. Simple Linear Regression/Simple_linear_regression.R
@@ -8,3 +8,36 @@ split = sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
+#Step-2 Fitting simple Linear Regression to the training dataset
+
+regressor = lm(formula = Salary ~ YearsExperience, data = training_set)
+
+#Step-3 Predict Test set
+
+y_pred = predict(regressor, newdata = test_set)
+
+#Step-4 
+# A) Visualizing training Set Results
+install.packages('ggplot2')
+library(ggplot2)
+
+ggplot() + 
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary)
+             , color = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set))
+            , color = 'blue') +
+  ggtitle('Salary vs Years of Experience(Training set)') + 
+  xlab('Years of Experience') +
+  ylab('Salary')
+# B) Visualizing Test Set Results
+library(ggplot2)
+
+ggplot() + 
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary)
+             , color = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set))
+            , color = 'blue') +
+  ggtitle('Salary vs Years of Experience(Test set)') + 
+  xlab('Years of Experience') +
+  ylab('Salary')
+
