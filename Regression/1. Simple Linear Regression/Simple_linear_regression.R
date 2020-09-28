f10a9f9f502c2ca6b24fc7380c1a18a272997811
SARTHAK ASTHANA <sarthakasthana2014@gmail.com>
2020-09-28 21:17:28 +0530

    DP done

---
+++ Regression/1. Simple Linear Regression/Simple_linear_regression.R
@@ -0,0 +1,10 @@
+#Step-1 Data Preprocessing
+
+dataset = read.csv('Salary_Data.csv')
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
